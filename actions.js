e44366efbe40e05ad9a8361b4edb3eace5c884b0
ArkadiuszGarzel <ejkej87@gmail.com>
2019-04-16 18:09:10 +0200

    Create actions in redux

---
+++ actions.js
@@ -0,0 +1,53 @@
+import uuid from uuid;
+
+const ADD_COMMENT = 'ADD_COMMENT';
+const EDIT_COMMENT = 'EDIT_COMMENT';
+const REMOVE_COMMENT = 'REMOVE_COMMENT';
+const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+function addComment(text) {
+    return {
+        type: ADD_COMMENT,
+        text,
+        id: uuid.v4()
+    }
+}
+
+function removeComment(text) {
+    return {
+        type: REMOVE_COMMENT,
+        text,
+        id: uuid.v4()
+    }
+}
+
+function editComment(id, newText){
+    return {
+        type:EDIT_COMMENT,
+        id,
+        text: newText
+    }
+}
+
+function thumbUpComment(id, thumb) {
+    return {
+        type: THUMB_UP_COMMENT,
+        id,
+        thumb: thumb++
+    }
+}
+
+function thumbDownComment(id, thumb) {
+    return {
+        type: THUMB_DOWN_COMMENT,
+        id,
+        thumb: thumb--
+    }
+}
+
+const boundAddComment = text => dispatch(addComment(text));
+const boundEditComment = (id, newText) => dispatch(editComment(id, newText));
+const boundRemoveComment = id => dispatch(removeComment(id));
+const boundThumbUpComment = (id, thumb) => dispatch(thumbUpComment(id, thumb));
+const boundThumbDownComment = (id, thumb) => dispatch(thumbDownComment(id, thumb));
